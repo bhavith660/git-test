80bc12f8fa5d0a254549cfbba6fc52e8c654dc8b
bhavith660 <bhavithganiga464@gmail.com>
2025-05-29 10:22:51 +0530

    Add files via upload

---
+++ test/App.java
@@ -0,0 +1,13 @@
+package egprogram4.prgm4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
